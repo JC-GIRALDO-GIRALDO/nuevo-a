37dc25b402e18f3a1bd9ba7dd8bab1a5c1c21ad3
JC-GIRALDO-GIRALDO <kmilom19@gmail.com>
2023-05-18 19:48:04 -0500

    aprendiendo 1 clase

---
+++ js/constantes.js
@@ -0,0 +1,5 @@
+export class persona {
+  constructor(nombre, apellido, edad, genero) {
+    nombre, apellido, edad, genero;
+  }
+}
---
+++ js/funciones.js
@@ -0,0 +1,4 @@
+import { persona } from "./constantes";
+export const Juan = new persona(juan, giraldo, 27, masculino);
+
+console.log(Juan);
